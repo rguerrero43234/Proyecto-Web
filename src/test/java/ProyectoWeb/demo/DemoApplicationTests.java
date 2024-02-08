955ddc9bed9cec1bd081c3800e6968964490b7de
rguerrero43234 <r.guerrero43234@gmail.com>
2024-02-07 18:34:26 -0600

    first commit

---
+++ src/test/java/ProyectoWeb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoWeb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
